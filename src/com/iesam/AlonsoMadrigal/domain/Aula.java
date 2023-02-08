42105a8a2617bffe14e67fac86ec405012e8875e
DerekLopezGarcia <115727176+DerekLopezGarcia@users.noreply.github.com>
2023-02-08 20:55:17 +0100

    Creadas las clases de Asignatura, Aula, Docencia y Ordenadores

---
+++ src/com/iesam/AlonsoMadrigal/domain/Aula.java
@@ -0,0 +1,31 @@
+package com.iesam.AlonsoMadrigal.domain;
+
+public class Aula {
+    private String id;
+    private String planta;
+    private String capacidad;
+
+    public String getPlanta() {
+        return planta;
+    }
+
+    public void setPlanta(String planta) {
+        this.planta = planta;
+    }
+
+    public String getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(String capacidad) {
+        this.capacidad = capacidad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
